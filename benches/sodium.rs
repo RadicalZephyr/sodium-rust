66e6e3bbc22add6bf2824e83760fa8739134e315
Zefira Shannon <earthlingzephyr@gmail.com>
2024-03-03 08:58:35 -0800

    Update crate name in benchmarks

--- benches/sodium.rs
+++ benches/sodium.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use sodium_rust::SodiumCtx;
+use sodium::SodiumCtx;
 
 fn stream(c: &mut Criterion) {
     let mut stream_send = c.benchmark_group("Stream::send");
